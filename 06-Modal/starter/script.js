7e727e7588aa4bf7961aaa951b616c05f6b97300
ioumesh <kuwar51@yahoo.com>
2021-07-27 20:43:33 +0530

    escape button added

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,19 +1,28 @@
 'use strict';
-const btnshow=document.querySelectorAll('.show-modal');
-const modal=document.querySelector('.modal');
-const overlay=document.querySelector('.overlay');
-const btnClose=document.querySelector('.close-modal');
+const btnshow = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
 
-const modalRemove=()=>{
+const modalRemove = () => {
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden')
 }
-const modalAdd=()=>{
+const modalAdd = () => {
     modal.classList.add('hidden')
     overlay.classList.add('hidden')
 }
-for(let i=0; i<btnshow.length; i++){
+for (let i = 0; i < btnshow.length; i++) {
     console.log(btnshow[i].addEventListener('click', modalRemove))
 }
 btnClose.addEventListener('click', modalAdd)
 overlay.addEventListener('click', modalAdd)
+
+document.addEventListener('keydown', function (e) {
+    console.log(e.key)
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+
+        modalAdd()
+
+    }
+})
