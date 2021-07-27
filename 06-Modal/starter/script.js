157861184a198cd69084aa3b24d22ae6c6ac4854
ioumesh <kuwar51@yahoo.com>
2021-07-27 18:50:25 +0530

    ui modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+const btnshow=document.querySelectorAll('.show-modal');
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnClose=document.querySelector('.close-modal');
+
+const modalRemove=()=>{
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+const modalAdd=()=>{
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+for(let i=0; i<btnshow.length; i++){
+    console.log(btnshow[i].addEventListener('click', modalRemove))
+}
+btnClose.addEventListener('click', modalAdd)
+overlay.addEventListener('click', modalAdd)
